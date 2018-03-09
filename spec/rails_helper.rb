858918924a4b716a6c805bd8c85b13fe96be3b98
leodotng <josh@leo.ng>
2018-03-08 19:57:56 -0700

    rails_helper.rb support directory autoload

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -35,6 +35,7 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configuration do |config|
+  config.include RequestSpecHelper, type: :request
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
