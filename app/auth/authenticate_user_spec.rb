492d63bae217d5ace81fc0f3042e909b9f888403
leodotng <josh@leo.ng>
2018-03-11 22:42:26 -0600

    return token when valid request

--- app/auth/authenticate_user_spec.rb
+++ app/auth/authenticate_user_spec.rb
@@ -6,3 +6,14 @@ RSpec.describe AuthenticateUser do
     subject(:valid_auth_obj) { described_class.new(user.email, user.password) }
 
     subject(:invalid_auth_obj) { described_class.new('foo', 'bar') }
+
+    describe '#call' do
+
+        context 'when valid credentials' do
+            it 'returns an auth token' do
+                token = valid_auth_obj.call
+                expect(token).not_to be_nil
+            end
+        end
+
+        
