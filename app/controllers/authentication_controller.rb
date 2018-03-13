1cbc6a6d3b52853ba631cd047a4ee40f95e319ac
leodotng <josh@leo.ng>
2018-03-12 19:31:13 -0600

    skip_before_action on authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,5 @@
 class AuthenticationController < ApplicationController
+    skip_before_action :authorize_request, only: :authenticate
     def Authenticate 
         auth_token = 
             AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
